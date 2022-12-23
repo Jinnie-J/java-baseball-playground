3d144fcc1f5c055e235cee6048a545f2d112f12a
Jinnie-J <jjesweet@naver.com>
2022-12-23 12:00:20 +0900

    2.숫자 야구 게임 구현 2

---
+++ src/test/java/baseballs/BallsTest.java
@@ -0,0 +1,59 @@
+package baseballs;
+
+import baseball.Ball;
+import baseball.BallStatus;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BallsTest {
+
+    @Test
+    void play_nothing(){
+        Balls answer = new Balls(Arrays.asList(1,2,3));
+        PlayResult result = answer.play(Arrays.asList(4,5,6));
+        assertThat(result.getStrike()).isEqualTo(0);
+        assertThat(result.getBall()).isEqualTo(0);
+    }
+
+    @Test
+    void play_1strike_1ball(){
+        Balls answer = new Balls(Arrays.asList(1,2,3));
+        PlayResult result = answer.play(Arrays.asList(3,2,5));
+        assertThat(result.getStrike()).isEqualTo(1);
+        assertThat(result.getBall()).isEqualTo(1);
+    }
+
+    @Test
+    void play_3strike(){
+        Balls answer = new Balls(Arrays.asList(1,2,3));
+        PlayResult result = answer.play(Arrays.asList(1,2,3));
+        assertThat(result.isGameEnd()).isTrue();
+    }
+
+
+    @Test
+    void strike(){
+        Balls answers = new Balls(Arrays.asList(1,2,3));
+        BallStatus status = answers.play(new Ball(1,1));
+        assertThat(status).isEqualTo(BallStatus.STRIKE);
+    }
+
+    @Test
+    void ball(){
+        Balls answers = new Balls(Arrays.asList(1,2,3));
+        BallStatus status = answers.play(new Ball(1,2));
+        assertThat(status).isEqualTo(BallStatus.BALL);
+    }
+
+    @Test
+    void nothing(){
+        Balls answers = new Balls(Arrays.asList(1,2,3));
+        BallStatus status = answers.play(new Ball(1,4));
+        assertThat(status).isEqualTo(BallStatus.NOTHING);
+    }
+
+
+}
